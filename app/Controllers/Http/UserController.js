8bf8426e176f19aeb6e5537e79d414744c828936
encodedBicoding <dominicisioma000@gmail.com>
2020-02-18 09:24:53 +0100

    Implements user ability to login and register on the platform

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,14 +3,29 @@
 const User = use("App/Models/User");
 
 class UserController {
-  async register({ request, response }) {
-    const userData = request.only(["email", "password", "username"]);
+  async register({ request, response, auth }) {
+    let userData = request.input(["username"]);
+    userData = userData.trim();
     try {
-      const user = await User.create(userData);
-      return response.json({
-        status: "success",
-        data: user
-      });
+      const isFound = await User.findByOrFail('username', userData);
+      if (!isFound) {
+        const new_user = await User.create({
+          username: userData
+        });
+        const token = await auth.generate(new_user, { jwtPayload: true })
+        return response.json({
+          status: 'success',
+          message: 'New user created successfully',
+          data: new_user,
+          session_token: token,
+        })
+      } else {
+        response.json({
+          status: 'failure',
+          message: `User with username: ${userData} already exists`,
+          data: {},
+        })
+      }
     } catch (error) {
       return response.status(400).json({
         status: "error",
@@ -21,10 +36,23 @@ class UserController {
   }
   async login({ request, auth, response }) {
     try {
-      await auth.attempt(request.input("email"), request.input("password"));
+      // await auth.attempt(request.input("email"), request.input("password"));
+      const isFound = await User.findBy('username', request.input('username'))
+      if (!isFound.user) {
+        return response.json({
+          status: 'failure',
+          message: 'User doesn\'t exists. Please register',
+          data: {},
+        })
+      }
+      let user = isFound.user;
+      // generate token
+      let token = await auth.generate(user, { jwtPayload: true})
       return response.json({
         status: "success",
-        data: auth.user
+        message: 'Log in successful',
+        data: user,
+        session_token: token
       });
     } catch (error) {
       response.status(400).json({
