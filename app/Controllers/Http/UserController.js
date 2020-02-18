9788a8d64d78bbf39a228ea347cede6f3da30889
encodedBicoding <dominicisioma000@gmail.com>
2020-02-18 12:19:44 +0100

    Implements user registeration and login using one function

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,62 +4,31 @@ const User = use("App/Models/User");
 
 class UserController {
   async register({ request, response, auth }) {
-    let userData = request.input(["username"]);
-    userData = userData.trim();
+    const { username } = request.post();
+    const userData  = username.trim();
     try {
-      const isFound = await User.findByOrFail('username', userData);
-      if (!isFound) {
-        const new_user = await User.create({
-          username: userData
-        });
-        const token = await auth.generate(new_user, { jwtPayload: true })
-        return response.json({
-          status: 'success',
-          message: 'New user created successfully',
-          data: new_user,
-          session_token: token,
-        })
-      } else {
-        response.json({
-          status: 'failure',
-          message: `User with username: ${userData} already exists`,
-          data: {},
-        })
-      }
+      const user = await User.create({
+        username: userData
+      });
+      console.log('Userd',user)
+      let token = await auth.generate(user)
+      return response.json({
+        status: 'success',
+        message: 'User log in successful',
+        data: user,
+        token
+      })
     } catch (error) {
+      console.log(error)
       return response.status(400).json({
         status: "error",
-        message:
-          "There was a problem creating the user, please try again later."
+        message: error.message,
+        data: {}
       });
     }
   }
   async login({ request, auth, response }) {
-    try {
-      // await auth.attempt(request.input("email"), request.input("password"));
-      const isFound = await User.findBy('username', request.input('username'))
-      if (!isFound.user) {
-        return response.json({
-          status: 'failure',
-          message: 'User doesn\'t exists. Please register',
-          data: {},
-        })
-      }
-      let user = isFound.user;
-      // generate token
-      let token = await auth.generate(user, { jwtPayload: true})
-      return response.json({
-        status: "success",
-        message: 'Log in successful',
-        data: user,
-        session_token: token
-      });
-    } catch (error) {
-      response.status(400).json({
-        status: "error",
-        message: error.message
-      });
-    }
+    // 
   }
   async logout({ auth, request, response }) {
     try {
